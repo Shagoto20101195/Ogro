bbf5aaa4855c411f1b64c14b09b0f0c63e5a995f
Andalib Rahman Shagoto <andalibrahman369@gmail.com>
2022-04-18 02:07:20 +0600

    Ok

--- validate.php
+++ validate.php
@@ -1,8 +1,4 @@
 <?php
     require_once("dbconnection.php");
     echo("Hello and welcome to our vast community -_-");
-<<<<<<< HEAD
 ?>
-=======
-?>
->>>>>>> e2e134bbb2c06228265c9cb28f603ef7436f68db
