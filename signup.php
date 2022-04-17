aee1cd841d15387c48b9dc26bd2aea406d61245f
Andalib Rahman Shagoto <andalibrahman369@gmail.com>
2022-04-18 02:17:52 +0600

    Signup page changed

---
+++ signup.php
@@ -0,0 +1,39 @@
+<?php
+    session_start();
+    include("dbconnection.php");
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <link rel="stylesheet" href="style.css" type="text/css">
+    <link rel="icon" href="images/Tom_evil_smile.jpg">
+    <title>Ogro - Signup</title>
+</head>
+    
+<body>
+    <header>
+        <div class="top-section">
+            <h1><abbr title="Online Agro">OGRO</abbr></h1>
+            <h3><i>Intended to serve a community of animal buyers and sellers across the country</i></h3>
+        </div>  
+    </header>
+
+    <main>
+        <div class="middle-section">
+            
+         </div>
+    </main>
+
+    <footer>
+        <div class="bottom-section">
+            <p>&copy; Unknown</p>
+            <span><a href="#" class="home-logout">Back To Top</a></span>
+            |
+            <span><a href="index.html" class="home-logout">Back To Home</a></span>
+        </div>
+    </footer>
+</body>
+</html>
